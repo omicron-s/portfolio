26bb2798ffbd693f664d684d5f677b7d99131ed1
Omicron <Omigod@live.ru>
2020-03-12 12:54:01 +0300

    Section-portfolio

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,7 @@
-
+// portfolio__pagination ul li active
+$(".portfolio__pagination a").click(function(e) {
+  e.preventDefault();
+  $(".portfolio__pagination a").removeClass("active");
+  $(this).addClass("active");
+});
+// --portfolio__pagination ul li active
