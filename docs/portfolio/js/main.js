5e9a8e45660bee3bd6b86ff5c4f6ab807b8b94b1
Omicron <Omigod@live.ru>
2020-03-15 18:24:46 +0300

    I did it!

--- docs/portfolio/js/main.js
+++ docs/portfolio/js/main.js
@@ -1,84 +1,41 @@
-// portfolio__pagination ul li active
-// $(".portfolio__pagination a").click(function(e) {
-//   e.preventDefault();
-//   $(".portfolio__pagination a").removeClass("active");
-//   $(this).addClass("active");
-// });
 
-$(".portfolio__pagination label").keyup(function(event) {
-  if (event.keyCode == 13) {
-    $(".portfolio__pagination label:focus").click();
-  }
+// portfolio__pagination ul li active
+$(".portfolio__pagination a").click(function(e) {
+  e.preventDefault();
+  $(".portfolio__pagination a").removeClass("active");
+  $(this).addClass("active");
 });
 
-// portfolio__pagination ul li active
-var selectAll;
-var bootstrap;
-var wordpress;
-var frontend;
+// $(".portfolio__pagination label").keyup(function(event) {
+//   if (event.keyCode == 13) {
+//     $(".portfolio__pagination label:focus").click();
+//   }
+// });
+
+var bootstrap = $(".portfolio-card--bootstrap");
+var wordpress = $(".portfolio-card--wordpress");
+var frontend = $(".portfolio-card--frontend");
 
 $("#select-all").click(function() {
   bootstrap.appendTo(".portfolio-row");
   wordpress.appendTo(".portfolio-row");
   frontend.appendTo(".portfolio-row");
-  bootstrap = null;
-  wordpress = null;
-  frontend = null;
 });
 
 $("#select-bootstrap").click(function() {
-  if (bootstrap) {
-    bootstrap.appendTo(".portfolio-row");
-    bootstrap = null;
-  }
-  bootstrap = $(".portfolio-card--bootstrap");
-  if (wordpress == null) {
-    wordpress = $(".portfolio-card--wordpress").detach();
-  }
-  if (frontend == null) {
-    frontend = $(".portfolio-card--frontend").detach();
-  }
+  bootstrap.appendTo(".portfolio-row");
+  $(".portfolio-card--wordpress").remove();
+  $(".portfolio-card--frontend").remove();
 });
 
 $("#select-wordpress").click(function() {
-  if (wordpress) {
-    wordpress.appendTo(".portfolio-row");
-    wordpress = null;
-  }
-  if (bootstrap == null) {
-    bootstrap = $(".portfolio-card--bootstrap").detach();
-  }
-  wordpress = $(".portfolio-card--wordpress");
-  if (frontend == null) {
-    frontend = $(".portfolio-card--frontend").detach();
-  }
+  $(".portfolio-card--bootstrap").remove();
+  wordpress.appendTo(".portfolio-row");
+  $(".portfolio-card--frontend").remove();
 });
 
 $("#select-frontend").click(function() {
-  if (frontend) {
-    frontend.appendTo(".portfolio-row");
-    frontend = null;
-  }
-  if (bootstrap == null) {
-    bootstrap = $(".portfolio-card--bootstrap").detach();
-  }
-  if (wordpress == null) {
-    wordpress = $(".portfolio-card--wordpress").detach();
-  }
-  frontend = $(".portfolio-card--frontend");
+  $(".portfolio-card--bootstrap").remove();
+  $(".portfolio-card--wordpress").remove();
+  frontend.appendTo(".portfolio-row");
 });
-
-// $("#select-bootstrap").click(function() {
-//   if (bootstrap) {
-//     bootstrap.appendTo(".portfolio-row");
-//     bootstrap = null;
-//   } else {
-//     bootstrap = $(".portfolio-card--bootstrap").detach();
-//   }
-// });
-
-// $("#select-all").click(function() {
-
-//   }
-// });
-// --portfolio__pagination ul li active
