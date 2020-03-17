a91cb667eaf5c28c6c33152162c501f529269a44
Omicron <Omigod@live.ru>
2020-03-17 06:54:58 +0300

    Add nav and animations in planning

--- docs/portfolio/js/main.js
+++ docs/portfolio/js/main.js
@@ -1,25 +1,26 @@
-
+// page-nav
+$("#page-nav").onePageNav({
+  currentClass: "current",
+  changeHash: false,
+  scrollThreshold: 0.4,
+});
 // portfolio__pagination ul li active
 $(".portfolio__pagination a").click(function(e) {
   e.preventDefault();
   $(".portfolio__pagination a").removeClass("active");
   $(this).addClass("active");
 });
-
-// $(".portfolio__pagination label").keyup(function(event) {
-//   if (event.keyCode == 13) {
-//     $(".portfolio__pagination label:focus").click();
-//   }
-// });
-
+// portfolio__pagination sort
+var selectAll = $(".portfolio-card");
 var bootstrap = $(".portfolio-card--bootstrap");
 var wordpress = $(".portfolio-card--wordpress");
 var frontend = $(".portfolio-card--frontend");
 
 $("#select-all").click(function() {
-  bootstrap.appendTo(".portfolio-row");
-  wordpress.appendTo(".portfolio-row");
-  frontend.appendTo(".portfolio-row");
+  $(".portfolio-card--bootstrap").remove();
+  $(".portfolio-card--wordpress").remove();
+  $(".portfolio-card--frontend").remove();
+  selectAll.appendTo(".portfolio-row");
 });
 
 $("#select-bootstrap").click(function() {
@@ -39,3 +40,4 @@ $("#select-frontend").click(function() {
   $(".portfolio-card--wordpress").remove();
   frontend.appendTo(".portfolio-row");
 });
+
